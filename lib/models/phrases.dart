5408e7895cfe7af9c848d39bb6cf4aa3973744b2
Emmy Etah <N1254885@my.ntu.ac.uk>
2025-07-31 04:28:26 +0100

    Created phrases.dart file to load .json file data.

---
+++ lib/models/phrases.dart
@@ -0,0 +1,23 @@
+class Phrase {
+  final String english;
+  final String oshikwanyama;
+  final String category;
+  final String audioFile;
+
+  Phrase ({
+    required this.english,
+    required this.oshikwanyama,
+    required this.category,
+    required this.audioFile,
+});
+
+factory Phrase.fromJson(Map<String,dynamic> json) {
+  return Phrase(
+    english: json['english'],
+    oshikwanyama: json['oshikwanyama'],
+    category: json['category'],
+    audioFile: json['audio_file'],
+  );
+}
+
+}
